5a080aa0bb240b2777682ad3b2082277c7b96339
Vinicius Hansel <hanelvinicius@gmail.com>
2025-07-05 15:48:57 -0300

    test: test get appointments route

--- src/test/java/com/hansel/FriseurPlan/core/application/usecase/appointment/query/AppointmentQueryUseCaseTest.java
+++ src/test/java/com/hansel/FriseurPlan/core/application/usecase/appointment/query/AppointmentQueryUseCaseTest.java
@@ -2,6 +2,8 @@ package com.hansel.FriseurPlan.core.application.usecase.appointment.query;
 
 import com.hansel.FriseurPlan.core.application.adapter.appointment.query.AppointmentQueryClient;
 import com.hansel.FriseurPlan.core.application.usecase.appointment.dto.AppointmentReturnDto;
+import com.hansel.FriseurPlan.core.application.usecase.appointment.dto.GetAppointmentDto;
+import com.hansel.FriseurPlan.core.domain.appointment.Appointment;
 import com.hansel.FriseurPlan.core.domain.appointment.TimeRange;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,8 +11,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
@@ -25,13 +31,17 @@ class AppointmentQueryUseCaseTest {
     private AppointmentQueryUseCase appointmentQueryUseCase;
 
     private Long appointmentId;
+    private Long hairdresserId;
+    private Long costumerId;
     private TimeRange timeRange;
-
+    private GetAppointmentDto  getAppointmentDto;
 
     private AppointmentReturnDto appointmentReturnDto;
     @BeforeEach
     void setUp() {
         appointmentId = 1L;
+        hairdresserId = 1L;
+        costumerId = 1L;
         timeRange = TimeRange.create(LocalDateTime.MIN, LocalDateTime.MAX);
         appointmentReturnDto = new AppointmentReturnDto(
                 appointmentId,
@@ -39,6 +49,12 @@ class AppointmentQueryUseCaseTest {
                 null,
                 null
         );
+        getAppointmentDto = new GetAppointmentDto(
+                costumerId,
+                hairdresserId,
+                Pageable.ofSize(10)
+        );
+
     }
 
     @Test
@@ -52,4 +68,21 @@ class AppointmentQueryUseCaseTest {
         assertEquals(timeRange.getStartTime(),appointmentById.timeRange().getStartTime());
         assertEquals(timeRange.getEndTime(),appointmentById.timeRange().getEndTime());
     }
+
+    @Test
+    void shouldGetAppointments() {
+        PageImpl<AppointmentReturnDto> appointments = new PageImpl<>(List.of(appointmentReturnDto));
+        when(this.appointmentQueryClient.getAppointments(getAppointmentDto)).thenReturn(appointments);
+        Page<AppointmentReturnDto> appointmentsPage = this.appointmentQueryUseCase.getAppointments(getAppointmentDto);
+
+        verify(this.appointmentQueryClient).getAppointments(getAppointmentDto);
+        assertNotNull(appointmentsPage);
+        assertEquals(appointmentsPage.getTotalElements(), appointmentsPage.getTotalElements());
+        assertEquals(appointmentsPage.getTotalPages(), appointmentsPage.getTotalPages());
+        assertEquals(appointmentsPage.getNumber(), appointmentsPage.getNumber());
+        assertEquals(appointmentsPage.getNumberOfElements(), appointmentsPage.getNumberOfElements());
+        assertEquals(appointmentsPage.getContent(), appointmentsPage.getContent());
+    }
+
+
 }
--- src/test/java/com/hansel/FriseurPlan/infra/port/output/client/appointment/query/AppointmentQueryClientImplTest.java
+++ src/test/java/com/hansel/FriseurPlan/infra/port/output/client/appointment/query/AppointmentQueryClientImplTest.java
@@ -1,5 +1,7 @@
 package com.hansel.FriseurPlan.infra.port.output.client.appointment.query;
 
+import com.hansel.FriseurPlan.core.application.usecase.appointment.dto.AppointmentReturnDto;
+import com.hansel.FriseurPlan.core.application.usecase.appointment.dto.GetAppointmentDto;
 import com.hansel.FriseurPlan.core.domain.Address;
 import com.hansel.FriseurPlan.core.domain.PhoneNumber;
 import com.hansel.FriseurPlan.core.domain.appointment.Appointment;
@@ -7,6 +9,7 @@ import com.hansel.FriseurPlan.core.domain.appointment.TimeRange;
 import com.hansel.FriseurPlan.core.domain.costumer.Costumer;
 import com.hansel.FriseurPlan.core.domain.email.Email;
 import com.hansel.FriseurPlan.core.domain.hairdresser.Hairdresser;
+import com.hansel.FriseurPlan.infra.port.output.client.appointment.specifications.GetAppointmentByRelationSpecs;
 import com.hansel.FriseurPlan.infra.port.output.entities.AddressVo;
 import com.hansel.FriseurPlan.infra.port.output.entities.EmailVo;
 import com.hansel.FriseurPlan.infra.port.output.entities.appointment.AppointmentEntity;
@@ -20,10 +23,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -32,10 +41,14 @@ class AppointmentQueryClientImplTest {
 
     @Mock
     private AppointmentEntityRepository appointmentEntityRepository;
+    @Mock
+    private GetAppointmentByRelationSpecs  getAppointmentByRelationSpecs;
     @InjectMocks
     private AppointmentQueryClientImpl appointmentQueryClient;
 
     private Long appointmentId;
+    private Long hairdresserId;
+    private Long costumerId;
     private Email email;
     private PhoneNumber phoneNumber;
     private Address address;
@@ -46,11 +59,14 @@ class AppointmentQueryClientImplTest {
     private Appointment appointment;
     private AppointmentEntity appointmentEntity;
     private CostumerEntity costumerEntity;
+    private GetAppointmentDto  getAppointmentDto;
 
 
     @BeforeEach
     void setUp() {
         this.appointmentId = 1L;
+        this.hairdresserId = 1L;
+        this.costumerId = 1L;
         this.email = Email.create("hanelvinicius@gmail.com", true);
         this.phoneNumber = PhoneNumber.create("16992977903");
         this.address = Address.create("123 Main St", 100, "Springfield", "SP", 12345678L);
@@ -61,6 +77,12 @@ class AppointmentQueryClientImplTest {
         this.appointment = Appointment.create(null,timeRange,this.costumer,this.hairdresser);
         this.costumerEntity  = new CostumerEntity(null,"name",phoneNumber,EmailVo.fromEmailDomain(email),LocalDateTime.MAX,null,null);
         this.appointmentEntity = new AppointmentEntity(null, TimeRangeVo.fromTimeRangeDomain(timeRange),hairdresserEntity,costumerEntity,LocalDateTime.MAX,null,null);
+        this.getAppointmentDto = new GetAppointmentDto(
+                costumerId,
+                hairdresserId,
+                Pageable.ofSize(10)
+        );
+
     }
 
     @Test
@@ -69,4 +91,16 @@ class AppointmentQueryClientImplTest {
         this.appointmentQueryClient.getAppointmentById(appointmentId);
         verify(this.appointmentEntityRepository).findById(appointmentId);
     }
+
+    @Test
+    public void getAppointments(){
+        PageImpl<AppointmentEntity> appointmentsPage = new PageImpl<>(List.of(this.appointmentEntity));
+        when(appointmentEntityRepository.findAll(any(GetAppointmentByRelationSpecs.class),any(Pageable.class))).thenReturn(appointmentsPage);
+        Page<AppointmentReturnDto> result = this.appointmentQueryClient.getAppointments(this.getAppointmentDto);
+        verify(this.appointmentEntityRepository).findAll(any(GetAppointmentByRelationSpecs.class),any(Pageable.class));
+        assertEquals(result.getTotalElements(), appointmentsPage.getTotalElements());
+        assertEquals(result.getTotalPages(), appointmentsPage.getTotalPages());
+        assertEquals(result.getContent().getFirst().id(), appointmentsPage.getContent().getFirst().getId());
+    }
+
 }
