981ae0861ca8ed25e0377293a24184b4389a435b
Vinicius Hansel <hanelvinicius@gmail.com>
2025-07-21 20:40:15 -0300

    chore: BaseIntegrationTest

---
+++ src/test/java/com/hansel/FriseurPlan/BaseIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.hansel.FriseurPlan;
+
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class BaseIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17-alpine")
+            .withUsername("root")
+            .withPassword("mariadbrootPW")
+            .withDatabaseName("friseurplan")
+            .withExposedPorts(5432);
+
+    @Container
+    static final KeycloakContainer keycloakContainer = new KeycloakContainer("quay.io/keycloak/keycloak:26.2")
+            .withRealmImportFile("test-realm-export.json");
+
+
+    @BeforeAll
+    static void beforeAll() {
+        postgresContainer.start();
+        keycloakContainer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgresContainer.stop();
+        keycloakContainer.stop();
+    }
+
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+        String host = keycloakContainer.getHost();
+        Integer port = keycloakContainer.getMappedPort(8080);
+        String issuerUri = "http://" + host + ":" + port + "/realms/FriseurPlanRealm";
+        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri", () -> issuerUri);
+        registry.add("spring.security.oauth2.client.provider.keycloak.issuer-uri", () -> issuerUri);
+    }
+}
--- src/test/java/com/hansel/FriseurPlan/infra/port/input/appointment/AppointmentControllerTest.java
+++ src/test/java/com/hansel/FriseurPlan/infra/port/input/appointment/AppointmentControllerTest.java
@@ -1,63 +1,17 @@
 package com.hansel.FriseurPlan.infra.port.input.appointment;
 
-import dasniko.testcontainers.keycloak.KeycloakContainer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import com.hansel.FriseurPlan.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.keycloak.admin.client.Keycloak;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.MountableFile;
 
 @SpringBootTest
 @Testcontainers
 @ActiveProfiles("test")
-class AppointmentControllerTest {
-
-    @Container
-    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17-alpine")
-            .withUsername("root")
-            .withPassword("mariadbrootPW")
-            .withDatabaseName("friseurplan")
-            .withExposedPorts(5432);
-
-    @Container
-    static final KeycloakContainer keycloakContainer = new KeycloakContainer("quay.io/keycloak/keycloak:26.2")
-            .withRealmImportFile("test-realm-export.json");
+class AppointmentControllerTest extends BaseIntegrationTest {
 
 
-
-
-    @BeforeAll
-    static void beforeAll() {
-        postgresContainer.start();
-        keycloakContainer.start();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        postgresContainer.stop();
-        keycloakContainer.stop();
-    }
-
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgresContainer::getUsername);
-        registry.add("spring.datasource.password", postgresContainer::getPassword);
-        String host = keycloakContainer.getHost();
-        Integer port = keycloakContainer.getMappedPort(8080);
-        String issuerUri = "http://" + host + ":" + port + "/realms/FriseurPlanRealm";
-        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri", () -> issuerUri);
-        registry.add("spring.security.oauth2.client.provider.keycloak.issuer-uri", () -> issuerUri);
-    }
 
     @Test
     public void shouldCreateAppointment() {
